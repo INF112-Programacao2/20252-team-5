83abc31287e547e16522431be210a2fda0d8b187
Valdinei-Stampini <valdinei.stampini@ufv.br>
2025-11-22 12:43:19 -0300

    Adicionei o arquivo personagem.cpp e fiz algumas alterações no .h correspondente

--- include/Personagem.h
+++ include/Personagem.h
@@ -1,26 +1,31 @@
 #ifndef PERSONAGEM_H
 #define PERSONAGEM_H
 
+#include <string>
 #include <SFML/Graphics.hpp>
 
 class Personagem
 {
 private:
-    int x;
-    int y;
-    int velocidade;
-    sf::Sprite sprite;
+    float _x;
+    float _y;
+    float _velocidade;
+    // coloquei float pois a movimentação que tentei fazer até agora é baseada nos pixels, 
+    // não nos quadrados da matriz, para evitar teleporte de um tile para outro. 
+    // Desse jeito ele passa de forma mais suave, mas dá para mudar, qualquer coisa
+    sf::Sprite _sprite;
 
 public:
-    Personagem(int x, int y, int velocidade, const sf::Texture &textura);
-    int getX() const;
-    int getY() const;
-    int getVelocidade() const;
+    Personagem(float x, float y, float velocidade, std::string imagem);
+    float getX() const;
+    float getY() const;
+    float getVelocidade() const;
+    sf::Sprite getSprite() const;
 
-    void setX(int novoX);
-    void setY(int novoY);
-    void setVelocidade(int novaVelocidade);
+    void setVelocidade(float novaVelocidade);
     void mudarPosicao(int novoX, int novoY);
+    bool colisao(Direcao direcao, float dist, unsigned char mapa[19][29]);
+    // esse mapa posteriormente deverá ser substituido por uma referencia para a fase
 };
 
 #endif
