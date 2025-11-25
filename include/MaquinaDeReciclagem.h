0d411d43fd1c9996c0bf74b2be154189754eac97
Carlos <carnot.br@gmail.com>
2025-11-24 21:55:48 -0300

    Declaração e implementação da classe MaquinaDeReciclagem

--- include/MaquinaDeReciclagem.h
+++ include/MaquinaDeReciclagem.h
@@ -1,8 +1,28 @@
 #ifndef MAQUINA_DE_RECICLAGEM_H
 #define MAQUINA_DE_RECICLAGEM_H
 
+class Fase;
+class Monstro;
+class Timer;
+
 class MaquinaDeReciclagem
 {
+    private:
+    int x;
+    int y;
+
+    Fase* fase;
+    Timer* timer;
+
+    public:
+    MaquinaDeReciclagem(int posX, int posY, Fase* _fase, Timer* _timer);
+
+    int getPosicaoX() const;
+    int getPosicaoY() const; 
+    
+    void receberInimigo(Monstro* inimigo); 
+
+    int calcularBonusTempo(Monstro* inimigo) const;
 };
 
 #endif
