06c5581b243e88984c3095c95057a1eebed71a84
caramu-jo <gabriel.silva16@ufv.br>
2025-11-28 09:27:56 -0300

    Update Monstro.h
    novos atributos: valorTempo e capturado. Mudança no construtor em relação a esses atributos. Adição de métodos: capturar e estaCapturado.

--- include/Monstro.h
+++ include/Monstro.h
@@ -4,16 +4,23 @@
 #include "Personagem.h"
 
 class Monstro : public Personagem {
+private:
+    int valorTempo; // tempo do bõnus
+    bool capturado; // Novo estado
 public:
     // Construtor simples para compilar
-    Monstro(float x, float y, float vel, std::string img) 
-        : Personagem(x, y, vel, img) {}
+    Monstro(float x, float y, float vel, std::string img, int valorTempoBonus) 
+        : Personagem(x, y, vel, img), valorTempo(valorTempoBonus), capturado(false) {}
         
     // Implementação obrigatória do método virtual puro
     void atualizar(float deltaTime) override { /* Lógica futura */ }
 
     // Método necessário para a Máquina de Reciclagem
-    int getValorTempo() const { return 10; } // Retorna 10 segundos de bônus, por exemplo
+    int getValorTempo() const { return valorTempo; } // Retorna o tempo em segundos de bônus
+
+    // Métodos para a mecânica de jogo
+    void capturar() { capturado = true; } // Muda o estado
+    bool estaCapturado() const { return capturado; } // Checa o estado
 };
 
 #endif
