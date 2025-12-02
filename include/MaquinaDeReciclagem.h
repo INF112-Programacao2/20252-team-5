3f60bd7afce4b162b23dbf4bc5e481af74b6df2a
raianysouza-exe <raiany.severino@ufv.br>
2025-12-02 09:19:24 -0300

    Update MaquinaDeReciclagem.h
    adição da função desenhar

--- include/MaquinaDeReciclagem.h
+++ include/MaquinaDeReciclagem.h
@@ -1,5 +1,7 @@
 #ifndef MAQUINA_DE_RECICLAGEM_H
 #define MAQUINA_DE_RECICLAGEM_H
+#include <SFML/Graphics.hpp>
+#include <string>
 
 class Fase;
 class Monstro;
@@ -7,22 +9,28 @@ class Timer;
 
 class MaquinaDeReciclagem
 {
-    private:
+private:
     int x;
     int y;
 
-    Fase* fase;
-    Timer* timer;
+    Fase *fase;
+    Timer *timer;
 
-    public:
-    MaquinaDeReciclagem(int posX, int posY, Fase* _fase, Timer* _timer);
+    sf::Sprite _sprite;
+    sf::Texture _texture;
+
+public:
+    MaquinaDeReciclagem(int posX, int posY, Fase *_fase, Timer *_timer, std::string imagem);
 
     int getPosicaoX() const;
-    int getPosicaoY() const; 
-    
-    void receberInimigo(Monstro* inimigo); 
+    int getPosicaoY() const;
+    sf::Sprite getSprite() const;
 
-    int calcularBonusTempo(Monstro* inimigo) const;
+    void desenhar(sf::RenderWindow &window);
+
+    void receberInimigo(Monstro *inimigo);
+
+    int calcularBonusTempo(Monstro *inimigo) const;
 };
 
 #endif
